f6d242ae87b29dab7c1500db48fdf597d80f913c
Kim Altintop <kim@users.noreply.github.com>
2017-11-20 22:53:55 +0100

    Add HTTPS support for CollectorReporter
    Note that this entails passing in the Manager

--- opentracing-jaeger/src/OpenTracing/Jaeger/CollectorReporter.hs
+++ opentracing-jaeger/src/OpenTracing/Jaeger/CollectorReporter.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ViewPatterns    #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE ViewPatterns      #-}
 
 module OpenTracing.Jaeger.CollectorReporter
     ( Env
@@ -35,24 +36,29 @@ import           Thrift.Transport.Empty
 type Env = BatchEnv
 
 data CollectorAddr = CollectorAddr
-    { collectorHost :: HostName
-    , collectorPort :: Port
+    { collectorHost   :: HostName
+    , collectorPort   :: Port
+    , collectorSecure :: Bool
     }
 
 defaultCollectorAddr :: CollectorAddr
 defaultCollectorAddr = CollectorAddr
-    { collectorHost = "127.0.0.1"
-    , collectorPort = 14268
+    { collectorHost   = "127.0.0.1"
+    , collectorPort   = 14268
+    , collectorSecure = False
     }
 
--- XXX: support https
-newEnv :: Text -> Tags -> CollectorAddr -> IO Env
-newEnv srv tags CollectorAddr{..} = do
-    rq  <- parseRequest $ "POST http://"
-               <> collectorHost <> ":" <> show collectorPort
-               <> "/api/traces?format=jaeger.thrift"
-    mgr <- newManager defaultManagerSettings
-    newBatchEnv 100 (reporter rq mgr (toThriftProcess srv tags))
+newEnv :: Manager -> Text -> Tags -> CollectorAddr -> IO Env
+newEnv mgr srv tags CollectorAddr{..} = do
+    rq <- mkReq
+    newBatchEnv 100 $ reporter rq mgr (toThriftProcess srv tags)
+  where
+    mkReq = do
+        rq <- parseRequest
+                    $ "http://" <> collectorHost <> ":" <> show collectorPort
+                   <> "/api/traces?format=jaeger.thrift"
+        pure rq { method = "POST", secure = collectorSecure }
+
 
 closeEnv :: Env -> IO ()
 closeEnv = closeBatchEnv
@@ -61,13 +67,14 @@ withEnv
     :: ( MonadIO   m
        , MonadMask m
        )
-    => Text
+    => Manager
+    -> Text
     -> Tags
     -> CollectorAddr
     -> (Env -> m a)
     -> m a
-withEnv srv tags addr =
-    bracket (liftIO $ newEnv srv tags addr) (liftIO . closeEnv)
+withEnv mgr srv tags addr =
+    bracket (liftIO $ newEnv mgr srv tags addr) (liftIO . closeEnv)
 
 
 jaegerCollectorReporter :: MonadIO m => Env -> FinishedSpan -> m ()
