0b5897ae5a300dd8826224c5bf6fa3119c679b91
Kim Altintop <kim@users.noreply.github.com>
2017-11-24 21:36:04 +0100

    Reconsider interaction between async exceptions and timing out the reporter action

--- opentracing/OpenTracing/Reporting.hs
+++ opentracing/OpenTracing/Reporting.hs
@@ -78,22 +78,25 @@ batchReporter :: MonadIO m => BatchEnv -> FinishedSpan -> m ()
 batchReporter BatchEnv{envQ} = liftIO . atomically . writeTQueue envQ
 
 consumer :: BatchOptions -> TQueue FinishedSpan -> IO (Async ())
-consumer opt@BatchOptions{..} q = async . handleAsync drain . forever $
-    go (void . atomically $ peekTQueue q)
+consumer opt@BatchOptions{..} q = async . forever $ do
+    w <- async $ go False
+    handleAsync drain $ do
+        wait w
+        void . atomically $ peekTQueue q
   where
-    go onEmpty = do
+    go draining = do
         batch <- atomically $ pop _boptBatchSize q
         case batch of
-            [] -> onEmpty
-            xs -> uninterruptibleMask_ $ do
-                      to <- timeout timeoutMicros
-                          . handleAny (logErr opt . ErrReporterException)
-                          $ _boptReporter xs
-                      case to of
-                          Nothing -> logErr opt ErrReporterTimeout
-                          Just _  -> return ()
+            [] -> return ()
+            xs -> do
+                r <- timeout timeoutMicros $
+                         _boptReporter xs
+                             `catchAny` (logErr opt . ErrReporterException)
+                case r of
+                    Nothing -> logErr opt ErrReporterTimeout
+                    Just () -> when draining $ go True
 
-    drain ThreadKilled = logErr opt ErrReporterCancelled *> go (return ())
+    drain ThreadKilled = logErr opt ErrReporterCancelled          *> go True
     drain e            = logErr opt (ErrReporterAsyncException e) *> throwM e
 
     timeoutMicros = micros @NominalDiffTime $ fromIntegral _boptTimeoutSec
@@ -123,7 +126,7 @@ logErr BatchOptions{_boptErrorLog=errlog} e = errlog $ msg e <> nl
     msg = \case
         ErrReporterException      ex -> sbs "Reporter Error: " <> ebs ex
         ErrReporterTimeout           -> sbs "Reporter timed out!"
-        ErrReporterCancelled         -> sbs "Batch reporter cancelled, commencing queue drain"
+        ErrReporterCancelled         -> sbs "Batch reporter cancelled"
         ErrReporterAsyncException ex -> sbs "Batch reporter received async exception: " <> ebs ex
 
     nl = char8 '\n'
