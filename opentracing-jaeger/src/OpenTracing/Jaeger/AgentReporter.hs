3c00cd49d8d2dceaf3234e19058f7ab002d01cb5
Kim Altintop <kim@users.noreply.github.com>
2017-11-27 15:11:26 +0100

    Use `Handle` thrift transport for agent

--- opentracing-jaeger/src/OpenTracing/Jaeger/AgentReporter.hs
+++ opentracing-jaeger/src/OpenTracing/Jaeger/AgentReporter.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE NamedFieldPuns    #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE StrictData        #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns             #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE StrictData                 #-}
+{-# LANGUAGE TemplateHaskell            #-}
 
 module OpenTracing.Jaeger.AgentReporter
     ( Options
@@ -24,48 +25,43 @@ module OpenTracing.Jaeger.AgentReporter
     )
 where
 
-import qualified Agent_Client                   as Thrift
+import qualified Agent_Client              as Thrift
 import           Control.Exception.Safe
-import           Control.Lens                   (makeLenses, view)
+import           Control.Lens              (makeLenses, view)
 import           Control.Monad.IO.Class
 import           Control.Monad.Reader
 import           Data.ByteString.Builder
 import           Data.Semigroup
-import           Data.Text                      (Text)
-import qualified Data.Vector                    as Vector
-import qualified Jaeger_Types                   as Thrift
+import           Data.Text                 (Text)
+import qualified Data.Vector               as Vector
+import qualified Jaeger_Types              as Thrift
 import           Network.Socket
-import           Network.Socket.ByteString.Lazy (sendAll)
 import           OpenTracing.Jaeger.Thrift
-import           OpenTracing.Reporting          (defaultErrorLog)
+import           OpenTracing.Reporting     (defaultErrorLog)
 import           OpenTracing.Span
 import           OpenTracing.Tags
 import           OpenTracing.Types
-import           Prelude                        hiding (span)
+import           Prelude                   hiding (span)
+import           System.IO
+    ( BufferMode (..)
+    , Handle
+    , IOMode (..)
+    , hSetBinaryMode
+    , hSetBuffering
+    )
 import qualified Thrift
-import qualified Thrift.Protocol.Compact        as Thrift
-import qualified Thrift.Transport.IOBuffer      as Thrift
+import qualified Thrift.Protocol.Compact   as Thrift
+import           Thrift.Transport.Handle   ()
 
 
 data Env = Env
     { envLocalProcess :: Thrift.Process
     , envErrorLog     :: Builder -> IO ()
-    , envTransport    :: UDPTrans
+    , envTransport    :: AgentTransport
     }
 
-data UDPTrans = UDPTrans
-    { udpSock :: Socket
-    , udpWBuf :: Thrift.WriteBuffer
-    , udpRBuf :: Thrift.ReadBuffer
-    }
-
-instance Thrift.Transport UDPTrans where
-    tIsOpen = isConnected . udpSock
-    tClose  = close . udpSock
-    tRead   = Thrift.readBuf  . udpRBuf
-    tWrite  = Thrift.writeBuf . udpWBuf
-    tPeek   = Thrift.peekBuf  . udpRBuf
-    tFlush UDPTrans{..} = Thrift.flushBuf udpWBuf >>= sendAll udpSock
+newtype AgentTransport = AgentTransport Handle
+    deriving Thrift.Transport
 
 data Options = Options
     { _optServiceName :: Text
@@ -73,8 +69,6 @@ data Options = Options
     , _optAddr        :: Addr 'UDP
     , _optErrorLog    :: Builder -> IO ()
     }
-
-makeLenses ''Options
 
 jaegerAgentOptions :: Text -> Options
 jaegerAgentOptions srv = Options
@@ -91,7 +85,7 @@ defaultAgentAddr = UDPAddr "127.0.0.1" 6831
 newEnv :: Options -> IO Env
 newEnv Options{..} =
     let tproc = toThriftProcess _optServiceName _optServiceTags
-     in Env tproc _optErrorLog <$> openUDPTrans _optAddr
+     in Env tproc _optErrorLog <$> openAgentTransport _optAddr
 
 closeEnv :: Env -> IO ()
 closeEnv Env{envTransport} = handleAny (const (return ())) $
@@ -100,16 +94,18 @@ closeEnv Env{envTransport} = handleAny (const (return ())) $
 withEnv :: (MonadIO m, MonadMask m) => Options -> (Env -> m a) -> m a
 withEnv opts = bracket (liftIO $ newEnv opts) (liftIO . closeEnv)
 
-openUDPTrans :: Addr 'UDP -> IO UDPTrans
-openUDPTrans addr = do
+openAgentTransport :: Addr 'UDP -> IO AgentTransport
+openAgentTransport addr = do
     AddrInfo{..} : _ <- getAddrInfo (Just defaultHints { addrSocketType = Datagram })
                                     (Just . view addrHostName $ addr)
                                     (Just . show . view addrPort $ addr)
     sock <- socket addrFamily addrSocketType addrProtocol
     connect sock addrAddress
+    hdl  <- socketToHandle sock ReadWriteMode
+    hSetBuffering  hdl NoBuffering -- MUST flush after every message!
+    hSetBinaryMode hdl True
 
-    UDPTrans sock <$> Thrift.newWriteBuffer <*> Thrift.newReadBuffer
-
+    return $ AgentTransport hdl
 
 jaegerAgentReporter :: MonadIO m => Env -> FinishedSpan -> m ()
 jaegerAgentReporter Env{..} s = liftIO $ emit `catchAny` err
@@ -120,3 +116,5 @@ jaegerAgentReporter Env{..} s = liftIO $ emit `catchAny` err
     err e = envErrorLog $ shortByteString "Jaeger Agent Thrift error: "
                        <> string8 (show e)
                        <> char8 '\n'
+
+makeLenses ''Options
