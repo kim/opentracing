c0d16fba2c62780ad7e80734efcbec3a2880ac8e
Kim Altintop <kim.altintop@gmail.com>
2018-02-02 14:15:09 +0100

    Expose non-bracket versions
    Appreciated by people who can't or don't want to use `MonadMask`, such
    as aficionados of `ExceptT`

--- opentracing/OpenTracing.hs
+++ opentracing/OpenTracing.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE BangPatterns          #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns        #-}
 {-# LANGUAGE RankNTypes            #-}
@@ -16,11 +15,17 @@ module OpenTracing
     , HasTracing(..)
     , runTracing
 
+    -- * 'MonadReader' interface
     , traced
-    , traced'
-
     , traced_
+    , startSpan
+    , finishSpan
+
+    -- * Interface requiring explicit threading of 'Tracing'
+    , traced'
     , traced'_
+    , startSpan'
+    , finishSpan'
     )
 where
 
@@ -77,52 +82,74 @@ traced_
     -> m a
 traced_ opt f = tracedResult <$> traced opt f
 
+startSpan
+    :: ( HasTracing  t
+       , MonadReader t m
+       , MonadIO       m
+       )
+    => SpanOpts
+    -> m ActiveSpan
+startSpan opt = view tracing >>= \t -> startSpan' t opt
+
+finishSpan
+    :: ( HasTracing  t
+       , MonadReader t m
+       , MonadIO       m
+       )
+    => ActiveSpan
+    -> m FinishedSpan
+finishSpan a = view tracing >>= \t -> finishSpan' t a
+
+
 traced'
-    :: ( MonadMask  m
+    :: ( HasTracing t
+       , MonadMask  m
        , MonadIO    m
-       , HasTracing t
        )
     => t
     -> SpanOpts
     -> (ActiveSpan -> m a)
     -> m (Traced a)
 traced' t opt f = do
-    let Tracing{traceStart} = view tracing t
-    span <- traceStart opt >>= liftIO . mkActive
-    ret  <- withException (f span) (onErr span)
-    fin  <- report span
+    span <- startSpan' t opt
+    -- /Note/: as per 'withException', we will be reporting any exception incl.
+    -- async ones. Exceptions thrown by 'finishSpan'' will be ignored, and the
+    -- one from 'f' will be rethrown. Observe that 'withException' does _not_
+    -- run the error handler under `uninterruptibleMask', unlike 'bracket'. This
+    -- is a good thing, as we might be doing blocking I/O.
+    ret  <- withException (f span) (onErr span >=> void . finishSpan' t)
+    fin  <- finishSpan' t span
     return Traced { tracedResult = ret, tracedSpan = fin }
   where
-    -- /Note/: as per 'withException', we will be reporting any exception incl.
-    -- async ones. Exceptions thrown by 'report' will be ignored, and the one
-    -- from 'f' will be rethrown. Observe that 'withException' does _not_ run
-    -- the error handler under `uninterruptibleMask', unlike 'bracket' -- this
-    -- is a good thing, as we might be doing blocking I/O.
-    onErr :: MonadIO m => ActiveSpan -> SomeException -> m ()
-    onErr span e = do
-        liftIO $ do
-            now <- getCurrentTime
-            modifyActiveSpan span $
-                  over spanTags (setTag (Error True))
-                . over spanLogs (LogRecord now (ErrObj e :| []) :)
-        void $ report span
-
-    report :: MonadIO m => ActiveSpan -> m FinishedSpan
-    report a = do
-        let Tracing{traceReport} = view tracing t
-        span <- liftIO (readActiveSpan a) >>= traceFinish
-        case view sampled span of
-            Sampled    -> traceReport span
-            NotSampled -> return () -- TODO: record metric
-        return span
+    onErr :: MonadIO m => ActiveSpan -> SomeException -> m ActiveSpan
+    onErr span e = liftIO $ do
+        now <- getCurrentTime
+        modifyActiveSpan span $
+              over spanTags (setTag (Error True))
+            . over spanLogs (LogRecord now (ErrObj e :| []) :)
+        pure span
 
 traced'_
-    :: ( MonadMask  m
+    :: ( HasTracing t
+       , MonadMask  m
        , MonadIO    m
-       , HasTracing t
        )
     => t
     -> SpanOpts
     -> (ActiveSpan -> m a)
     -> m a
 traced'_ t opt f = tracedResult <$> traced' t opt f
+
+startSpan' :: (HasTracing t, MonadIO m) => t -> SpanOpts -> m ActiveSpan
+startSpan' t opt = do
+    let Tracing{traceStart} = view tracing t
+    traceStart opt >>= liftIO . mkActive
+
+finishSpan' :: (HasTracing t, MonadIO m) => t -> ActiveSpan -> m FinishedSpan
+finishSpan' t a = do
+    let Tracing{traceReport} = view tracing t
+    span <- liftIO (readActiveSpan a) >>= traceFinish
+    case view sampled span of
+        Sampled    -> traceReport span
+        NotSampled -> return () -- TODO: record metric
+    return span
