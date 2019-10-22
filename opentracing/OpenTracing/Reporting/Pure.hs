e16cea858df52dd949f082b8911380c7e7b6101a
Ben Weitzman <benweitzman@gmail.com>
2019-10-22 14:09:09 -0400

    Docs for OpenTracing.Reporting sub modules

--- opentracing/OpenTracing/Reporting/Pure.hs
+++ opentracing/OpenTracing/Reporting/Pure.hs
@@ -1,3 +1,9 @@
+{-|
+Module: OpenTracing.Reporting.Pure
+
+Reporters with no external components.
+-}
+
 {-# LANGUAGE NamedFieldPuns  #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StrictData      #-}
@@ -17,21 +23,28 @@ import Data.IORef
 import Data.Word
 import OpenTracing.Span
 
-
+-- | A null reporter which ignores anything it's given.
 noReporter :: MonadIO m => FinishedSpan -> m ()
 noReporter = const $ pure ()
 
+-- | A reporter which stores the finished spans in memory where
+-- they wait to be consumed.
 memReporter :: MonadIO m => Mem -> FinishedSpan -> m ()
 memReporter m = liftIO . memAppend m
 
+-- | Mem reporter state.
 data Mem = Mem
     { siz :: Maybe Word32
     , vec :: IORef [FinishedSpan]
     }
 
+-- | Construct a new `memReporter` environment that can store an unbounded
+-- seequence of `FinishedSpan`s.
 newMem :: IO Mem
 newMem = Mem Nothing <$> newIORef []
 
+-- | Construct a new `memReprioerter` environment that stores a bounded
+-- sequence of `FinishedSpan`s
 newBoundedMem :: Word32 -> IO Mem
 newBoundedMem s = Mem (Just s) <$> newIORef []
 
@@ -44,8 +57,10 @@ memAppend Mem{..} x = atomicModifyIORef' vec $ \xs ->
                   Just  s -> x : take (fromIntegral s - 1) xs
      in (xs', ())
 
+-- | View the `FinishedSpans` in a `memReporter` without removing them.
 memPeek :: Mem -> IO [FinishedSpan]
 memPeek Mem{vec} = readIORef vec
 
+-- | View and remove the `FinishedSpans` in a `memReporter`.
 memTake :: Mem -> IO [FinishedSpan]
 memTake Mem{vec} = atomicModifyIORef' vec $ (,) []
